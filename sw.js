45ef73df48fe83767023a6b9a99474ca0115bcc4
farid77cl <56374383+farid77cl@users.noreply.github.com>
2026-06-22 15:22:43 -0400

    Sync seguro: login email/password + parser de config

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ayunka-studio-v8';
+const CACHE='ayunka-studio-v9';
 const ASSETS=['./','./index.html','./css/styles.css','./js/plan-data.js','./js/store.js','./js/idb.js','./js/calc.js','./js/filament-colors.js','./js/designs.js','./js/sync.js','./js/app.js','./js/pdf.js','./img/logo.png','./favicon.ico','./favicon.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
